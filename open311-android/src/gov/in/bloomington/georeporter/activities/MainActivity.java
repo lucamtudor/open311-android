953eb33e2207d7096d8d62f71b66ca7c5c08017a
Tudor Luca <lucamtudor@gmail.com>
2013-04-22 00:02:28 +0300

    Splash images have the same resolution. - added fill area for 9patch files; - splash images are loaded in the ImageView as backgroud resources, not   image resources;

--- open311-android/src/gov/in/bloomington/georeporter/activities/MainActivity.java
+++ open311-android/src/gov/in/bloomington/georeporter/activities/MainActivity.java
@@ -3,16 +3,15 @@
  * @license http://www.gnu.org/licenses/gpl.txt GNU/GPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
+
 package gov.in.bloomington.georeporter.activities;
 
 import gov.in.bloomington.georeporter.R;
 import gov.in.bloomington.georeporter.models.Open311;
 import gov.in.bloomington.georeporter.models.Preferences;
 import gov.in.bloomington.georeporter.util.Util;
-
 import gov.in.bloomington.georeporter.util.json.JSONException;
 import gov.in.bloomington.georeporter.util.json.JSONObject;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -24,79 +23,78 @@ public class MainActivity extends BaseActivity {
     private static String SPLASH_IMAGE = "splash_image";
 
     @Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-	}
-	
-	@Override
-	protected void onResume() {
-		super.onResume();
-		
-		JSONObject current_server = Preferences.getCurrentServer(this);
-		
-		if (current_server == null) {
-    		startActivity(new Intent(this, SettingsActivity.class));
-		}
-		else {
-			new EndpointLoader().execute(current_server);
-			
-			try {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        JSONObject current_server = Preferences.getCurrentServer(this);
+
+        if (current_server == null) {
+            startActivity(new Intent(this, SettingsActivity.class));
+        } else {
+            new EndpointLoader().execute(current_server);
+
+            try {
                 getSupportActionBar().setTitle(current_server.getString(Open311.NAME));
-            }
-            catch (JSONException e) {
+            } catch (JSONException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-			
+
             String imageName = current_server.optString(SPLASH_IMAGE);
             if (imageName != "") {
-                ImageView splash = (ImageView) findViewById(R.id.splash);
-                splash.setImageResource(getResources().getIdentifier(imageName, "drawable", getPackageName()));
+                ImageView splash = (ImageView)findViewById(R.id.splash);
+                int splashPictureId = getResources().getIdentifier(imageName, "drawable", getPackageName());
+                if (splashPictureId != 0) {
+                    splash.setBackgroundResource(splashPictureId);
+                }
                 try {
                     splash.setContentDescription(current_server.getString(Open311.NAME));
-                }
-                catch (JSONException e) {
+                } catch (JSONException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
             }
-		}
-	}
-	
-	/**
-	 * OnClick handler for activity_main layout
-	 * 
-	 * @param v
-	 * void
-	 */
-	public void onTouchImage(View v) {
+        }
+    }
+
+    /**
+     * OnClick handler for activity_main layout
+     * 
+     * @param v void
+     */
+    public void onTouchImage(View v) {
         Intent intent = new Intent(this, ReportActivity.class);
         startActivity(intent);
-	}
-	
-	private class EndpointLoader extends AsyncTask<JSONObject, Void, Boolean> {
-		private ProgressDialog dialog;
-		
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-			dialog = ProgressDialog.show(MainActivity.this, getString(R.string.dialog_loading_services), "", true);
-		}
-		
-		@Override
-		protected Boolean doInBackground(JSONObject... server) {
-			return Open311.setEndpoint(server[0], MainActivity.this);
-		}
-		
-		@Override
-		protected void onPostExecute(Boolean result) {
-			dialog.dismiss();
-			if (!result) {
-				Util.displayCrashDialog(MainActivity.this, getString(R.string.failure_loading_services));
-			}
-			super.onPostExecute(result);
-		}
-	}
+    }
+
+    private class EndpointLoader extends AsyncTask<JSONObject, Void, Boolean> {
+        private ProgressDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = ProgressDialog.show(MainActivity.this, getString(R.string.dialog_loading_services), "", true);
+        }
+
+        @Override
+        protected Boolean doInBackground(JSONObject... server) {
+            return Open311.setEndpoint(server[0], MainActivity.this);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            dialog.dismiss();
+            if (!result) {
+                Util.displayCrashDialog(MainActivity.this, getString(R.string.failure_loading_services));
+            }
+            super.onPostExecute(result);
+        }
+    }
 }
